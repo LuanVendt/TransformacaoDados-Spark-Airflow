6481d4acf99ac4fc63d9915ca76920923dec3f25
Luan Vendt <luanvendt@gmail.com>
2024-03-05 14:12:44 -0300

    Adicionando caminho relativo para datalake

--- airflow_pipeline/dags/twitter_dag.py
+++ airflow_pipeline/dags/twitter_dag.py
@@ -12,17 +12,23 @@ sys.path.append(parent_dir)
 from operators.twitter_operator import TwitterOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.utils.dates import days_ago
+from pathlib import Path
 
 
 with DAG(dag_id = 'TwitterDAG', start_date=days_ago(2), schedule_interval='@daily') as dag:
+    BASE_FOLDER = join(
+       str(Path("~/Documents").expanduser()),
+       "curso-extracao-de-dados/datalake/{stage}/twitter_datascience/{partition}",
+   )
+    PARTITION_FOLDER_EXTRACT = "extract_date={{ data_interval_start.strftime('%Y-%m-%d') }}"
+
     TIME_STAMP = "%Y-%m-%dT%H:%M:%S.00Z"
 
     end_time = datetime.now().strftime(TIME_STAMP)
     start_time = (datetime.now() + timedelta(-1)).date().strftime(TIME_STAMP)
     query = 'data science'
 
-    twitter_operator = TwitterOperator(file_path=join('datalake/bronze/twitter_datascience', 
-                                        'extract_date={{ ds }}',
+    twitter_operator = TwitterOperator(file_path=join(BASE_FOLDER.format(stage='bronze', partition=PARTITION_FOLDER_EXTRACT),
                                         'datascience_{{ ds_nodash }}.json'), 
                                         query=query, 
                                         start_time='{{ data_interval_start.strftime("%Y-%m-%dT%H:%M:%S.00Z") }}', 
@@ -34,9 +40,9 @@ with DAG(dag_id = 'TwitterDAG', start_date=days_ago(2), schedule_interval='@dail
                                 application='/home/luan/Documents/curso-extracao-de-dados/src/spark/transaformation.py',
                                 name='twitter_transformation',
                                 application_args=['--src', 
-                                                  'datalake/bronze/twitter_datascience', 
+                                                  BASE_FOLDER.format(stage='bronze', partition=PARTITION_FOLDER_EXTRACT), 
                                                   '--dest',
-                                                  'datalake/silver/twitter_datascience',
+                                                  BASE_FOLDER.format(stage='silver', partition=''), 
                                                   '--process-date',
                                                   '{{ ds }}'
                                                   ])
