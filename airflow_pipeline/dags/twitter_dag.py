809182e8c95361b8a784d5c4fb6de31750aa21b0
Luan Vendt <luanvendt@gmail.com>
2024-03-05 15:21:49 -0300

    twitter_insight twitter dag

--- airflow_pipeline/dags/twitter_dag.py
+++ airflow_pipeline/dags/twitter_dag.py
@@ -47,4 +47,12 @@ with DAG(dag_id = 'TwitterDAG', start_date=days_ago(2), schedule_interval='@dail
                                                   '{{ ds }}'
                                                   ])
     
-    twitter_operator >> twitter_transform
+    twitter_insight = SparkSubmitOperator(task_id="insight_twitter",
+                                        application="/home/luan/Documents/curso-extracao-de-dados/src/spark/insight_twitter.py",
+                                        name="insight_twitter",
+                                        application_args=["--src", BASE_FOLDER.format(stage="silver", partition=""),
+                                            "--dest", BASE_FOLDER.format(stage="gold", partition=""),
+                                            "--process-date", "{{ ds }}"])
+
+    
+    twitter_operator >> twitter_transform >> twitter_insight
