d73d2c865494162ee3ee0d3459a501c7a70631de
Luan Vendt <luanvendt@gmail.com>
2024-03-05 12:12:41 -0300

    twitter_transform SparkSubmitOperator

--- airflow_pipeline/dags/twitter_dag.py
+++ airflow_pipeline/dags/twitter_dag.py
@@ -5,6 +5,7 @@ from airflow.models import DAG
 from datetime import datetime, timedelta
 from os.path import join
 from operators.twitter_operator import TwitterOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.utils.dates import days_ago
 
 
@@ -15,10 +16,24 @@ with DAG(dag_id = 'TwitterDAG', start_date=days_ago(2), schedule_interval='@dail
     start_time = (datetime.now() + timedelta(-1)).date().strftime(TIME_STAMP)
     query = 'data science'
 
-    to = TwitterOperator(file_path=join('datalake/twitter_datascience', 
+    twitter_operator = TwitterOperator(file_path=join('datalake/twitter_datascience', 
                                         'extract_date={{ ds }}',
                                         'datascience_{{ ds_nodash }}.json'), 
                                         query=query, 
                                         start_time='{{ data_interval_start.strftime("%Y-%m-%dT%H:%M:%S.00Z") }}', 
                                         end_time='{{ data_interval_end.strftime("%Y-%m-%dT%H:%M:%S.00Z") }}', 
                                         task_id='twitter_datascience')
+    
+    twitter_transform = SparkSubmitOperator(
+                                task_id='transform_twitter_datascience',
+                                application='/home/luan/Documents/curso-extracao-de-dados/src/spark/transaformation.py',
+                                name='twitter_transformation',
+                                application_args=['--src', 
+                                                  '/home/luan/Documents/curso-extracao-de-dados/datalake/twitter_datascience', 
+                                                  '--dest',
+                                                  '/home/luan/Documents/curso-extracao-de-dados/dados_transformation',
+                                                  '--process-date',
+                                                  '{{ ds }}'
+                                                  ])
+    
+    twitter_operator >> twitter_transform
